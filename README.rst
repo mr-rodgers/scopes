5b9d92cde0e17c2e9b4aa2d7f459a757f705d764
Te-jé Rodgers <tjd.rodgers@gmail.com>
2016-06-03 11:38:37 -0400

    "initial commit"

---
+++ README.rst
@@ -0,0 +1,37 @@
+scopelist
+===============================
+
+|build-status| |license|
+
+A container class for authorization scopes
+
+Installation
+------------
+
+You can install scopelist directly from GitHub::
+
+    > pip install git+https://github.com/te-je/scopelist
+
+That should install all the dependencies for you. If you want to install
+directly from source, clone the git repository and run the standard
+`python setup.py install` command.
+
+Dependencies
+~~~~~~~~~~~~
+
+* Python 2.7+ (including Python 3, via 2to3)
+
+Usage
+-----
+
+* TODO
+
+.. |build-status| image:: https://travis-ci.org/te-je/scopelist.svg?branch=develop
+    :target: https://travis-ci.org/te-je/scopelist
+    :alt: build status
+    :scale: 100%
+
+.. |license| image:: https://img.shields.io/badge/license-MIT-blue.svg
+    :target: https://raw.githubusercontent.com/te-je/scopelist/develop/LICENSE.txt
+    :alt: License
+    :scale: 100%
---
+++ setup.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from os import path
+
+
+HERE = path.abspath(path.dirname(__file__))
+
+
+def read(relpath):
+    with open(path.join(HERE, relpath), encoding="utf-8") as f:
+        return f.read()
+
+setup(
+    name='scopelist',
+
+    description='A container class for authorization scopes',
+    long_description="\n\n".join([
+        read("README.rst"),
+        "License\n-------",
+        read("LICENSE.rst")
+    ]),
+
+    url='https://github.com/te-je/scopelist',
+
+    author='Te-jé Rodgers',
+    author_email='tjd.rodgers@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+
+    ],
+
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
+
+    packages=find_packages(exclude=['test']),
+    install_requires=[],
+
+    extras_require={},
+
+    package_data={
+        'scopelist': ['VERSION.txt'],
+    },
+
+    entry_points={},
+)
